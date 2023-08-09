55c9be727b1c5ba3138b374302a25f3cc1918c3e
lesponns <adamadakadam1903@gmail.com>
2023-08-09 14:17:20 +0300

    Add project files.

---
+++ SmartPro_25[08-08-23]/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_25_08_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, last = 0;
+            double avarage;
+            Console.Write("Limit: ");
+            input = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for (int i = 1; i <= input; i++) 
+            {
+                last += i; //last = last + i;
+            }
+            avarage = (double)last / input;
+            Console.WriteLine("1'den {0} sayısına kadar olan sayıların toplamı: {1}\nSayıların ortalaması: {2}",input,last,avarage);
+            Console.ReadKey();
+        }
+    }
+}
